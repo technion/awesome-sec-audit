4550543a18872496cbc6f7ab7afa44985024081d
Technion <technion@lolware.net>
2016-11-23 05:39:01 +0000

    Add new files.

---
+++ assets/awesome-sec-audit-run.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Start from "./awesome-sec-audit.tsx";
+
+// Dedicating a file to this basic call allows us to import all major modules
+// into test suites.
+ReactDOM.render(
+    <Start />,
+    document.getElementById("content")
+);
---
+++ tests/test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { mountToJson } from 'enzyme-to-json';
+import * as renderer from 'react-test-renderer';
+
+//import * as renderer from 'react-test-renderer';
+import Start from '../assets/awesome-sec-audit.tsx';
+
+test('render displays correctly', () => {
+
+    const component = mount(
+            <Start />
+    );
+
+    let tree = mountToJson(component);
+    expect(tree).toMatchSnapshot();
+
+    component.find("button").simulate("click");
+
+    tree = mountToJson(component);
+    expect(tree).toMatchSnapshot();
+
+    component.find("input").simulate("change", {target: {value: 30} });
+    component.find("button").simulate("click");
+    tree = mountToJson(component);
+    expect(tree).toMatchSnapshot();
+
+};
